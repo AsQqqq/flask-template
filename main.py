6a70668088de8042df0d76636b53e6b67e4629d4
AsQ <danila.udodov.workamil@gmail.com>
2023-09-28 13:16:39 +0300

    Добовление вкладки about

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def index() -> render_template:
     return render_template('index.html')
+
+@app.route('/about')
+def about() -> str: 
+    return "about"
 
 if __name__ == "__main__":
     app.run(debug=True)
