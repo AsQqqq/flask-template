5f6f62b8afc476775e4c7ffe5666f59bcb8cacd8
AsQ <danila.udodov.workamil@gmail.com>
2023-09-28 13:21:39 +0300

    Добовление about файла

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index() -> render_template:
+    """Основная вкладка"""
     return render_template('index.html')
 
 @app.route('/about')
 def about() -> str: 
-    return "about"
+    "О нас"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
